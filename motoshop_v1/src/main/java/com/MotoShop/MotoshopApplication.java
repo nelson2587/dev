38f4cfa7a9fe3915ba57019d2bdae0ff949adc50
nelson2587 <105759705+nelson2587@users.noreply.github.com>
2024-02-01 23:13:35 -0600

    motorepuestos87
    Es una pagina web dirigida a la venta de repuestos  y accesorios de motocicletas que incluya videos y música además de una lista de repuestos amplia y detallada

---
+++ motoshop_v1/src/main/java/com/MotoShop/MotoshopApplication.java
@@ -0,0 +1,13 @@
+package com.MotoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoshopApplication.class, args);
+	}
+
+}
---
+++ motoshop_v1/src/test/java/com/MotoShop/MotoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MotoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
