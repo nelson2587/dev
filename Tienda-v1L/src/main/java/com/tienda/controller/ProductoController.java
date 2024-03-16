7d621d791a26abe323bfaa285f082b95d5c7f967
nelson2587 <105759705+nelson2587@users.noreply.github.com>
2024-03-16 17:15:45 -0600

    Practica 4
    proyecto tienda ahora con el CRUD tanto de categorias como de productos

--- Tienda-v1L/src/main/java/com/tienda/controller/ProductoController.java
+++ Tienda-v1L/src/main/java/com/tienda/controller/ProductoController.java
@@ -1,8 +1,9 @@
 package com.tienda.controller;
 
 
-import com.tienda.domain.Categoria;
+import com.tienda.domain.Producto;
 import com.tienda.service.CategoriaService;
+import com.tienda.service.ProductoService;
 import com.tienda.service.impl.FirebaseStorageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,53 +15,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/categoria")
-public class CategoriaController {
+@RequestMapping("/producto")
+public class ProductoController {
   
     @Autowired
+    private ProductoService productoService;
     private CategoriaService categoriaService;
     
     @GetMapping("/listado")
     private String listado(Model model) {
+        var productos = productoService.getProductos(false);
         var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos",productos.size());
         model.addAttribute("categorias", categorias);
-        model.addAttribute("totalCategorias",categorias.size());
-        return "/categoria/listado";
+        return "/producto/listado";
     }
     
      @GetMapping("/nuevo")
-    public String categoriaNuevo(Categoria categoria) {
-        return "/categoria/modifica";
+    public String productoNuevo(Producto producto) {
+        return "/producto/modifica";
     }
 
     @Autowired
     private FirebaseStorageServiceImpl firebaseStorageService;
     
     @PostMapping("/guardar")
-    public String categoriaGuardar(Categoria categoria,
+    public String productoGuardar(Producto producto,
             @RequestParam("imagenFile") MultipartFile imagenFile) {        
         if (!imagenFile.isEmpty()) {
-            categoriaService.save(categoria);
-            categoria.setRutaImagen(
+            productoService.save(producto);
+            producto.setRutaImagen(
                     firebaseStorageService.cargaImagen(
                             imagenFile, 
-                            "categoria", 
-                            categoria.getIdCategoria()));
+                            "producto", 
+                            producto.getIdProducto()));
         }
-        categoriaService.save(categoria);
-        return "redirect:/categoria/listado";
+        productoService.save(producto);
+        return "redirect:/producto/listado";
     }
 
-    @GetMapping("/eliminar/{idCategoria}")
-    public String categoriaEliminar(Categoria categoria) {
-        categoriaService.delete(categoria);
-        return "redirect:/categoria/listado";
+    @GetMapping("/eliminar/{idProducto}")
+    public String productoEliminar(Producto producto) {
+        productoService.delete(producto);
+        return "redirect:/producto/listado";
     }
 
-    @GetMapping("/modifica/{idCategoria}")
-    public String categoriaModificar(Categoria categoria, Model model) {
-        categoria = categoriaService.getCategoria(categoria);
-        model.addAttribute("categoria", categoria);
-        return "/categoria/modifica";
+    @GetMapping("/modifica/{idProducto}")
+    public String productoModificar(Producto producto, Model model) {
+        producto = productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        return "/producto/modifica";
     }   
 }
