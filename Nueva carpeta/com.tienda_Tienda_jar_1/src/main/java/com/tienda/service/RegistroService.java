9b2a1bf1155843c9c5ca7ca8cedc75db76729e14
nelson2587 <105759705+nelson2587@users.noreply.github.com>
2024-04-21 22:00:09 -0600

    ultima actualizacion git
    proyecto tienda 2

---
+++ Nueva carpeta/com.tienda_Tienda_jar_1/src/main/java/com/tienda/service/RegistroService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.domain.Usuario;
+import jakarta.mail.MessagingException;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author Nelson
+ */
+public interface RegistroService {
+  public Model activar (Model model,String usurio,String clave);  
+  public Model crearUsuario (Model model,Usuario usurio) throws MessagingException;
+  public void activar (Usuario usuario, MultipartFile imagenFile);
+  public Model recordarUsuario (Model model,Usuario usurio) throws MessagingException;
+  
+}
